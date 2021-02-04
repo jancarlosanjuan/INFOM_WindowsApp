cc9a8248b563e7971cb1e0fb520ecdea583bdc82
jancarlosanjuan <58461098+jancarlosanjuan@users.noreply.github.com>
2021-02-04 23:33:02 +0800

    FIxed UI
    NICE

--- INFOM_FINAL_MP/INFOM_FINAL_MP/MainWindow.xaml.cs
+++ INFOM_FINAL_MP/INFOM_FINAL_MP/MainWindow.xaml.cs
@@ -19,14 +19,16 @@ namespace INFOM_FINAL_MP
         private void PlayerButton_Click(object sender, RoutedEventArgs e)
         {
             string playerId = SearchBar.Text;
-            User user = DBQuery.GetUserFromId(playerId);
+            //User user = DBQuery.GetUserFromId(playerId);
 
-            PlayerName.Text = user.SteamName;
-            PlayerKD.Text = user.KdRatio.ToString("F2");
-            PlayerKills.Text = user.TotalKills.ToString();
-            PlayerDeaths.Text = user.TotalDeaths.ToString();
-            PlayerWins.Text = user.TotalWins.ToString();
-            PlayerLosses.Text = user.TotalLosses.ToString();
+            //PlayerName.Text = user.SteamName;
+            // PlayerKD.Text = user.KdRatio.ToString("F2");
+            // PlayerKills.Text = user.TotalKills.ToString();
+            // PlayerDeaths.Text = user.TotalDeaths.ToString();
+            //  PlayerWins.Text = user.TotalWins.ToString();
+            //  PlayerLosses.Text = user.TotalLosses.ToString();
+            DataGrid.ItemsSource = DBQuery.GetUserFromName2(playerId).DefaultView;
+
         }
     }
 }
